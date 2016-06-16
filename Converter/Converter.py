d810ecff36c1a198a71a3d9b38e329085a5a3639
David <lawlor041988@gmail.com>
2016-06-16 02:49:58 +0100

    updated converter.py

--- Converter/Converter.py
+++ Converter/Converter.py
@@ -8,23 +8,23 @@ def convert_kph_to_mph(kilometers_in):
 
 
 def convert_mph_to_kph(miles_in):
-    print ""
+    print (miles_in, " Miles in kilometers is ", miles_in * 1.6093)
 
 
-def convert_centimeters_to_inches():
-    print ""
+def convert_centimeters_to_inches(centimeter_in):
+    print (centimeter_in, " centimeters in inches is ", centimeter_in * 0.39370)
 
 
-def convert_inches_to_centimeters():
-    print ""
+def convert_inches_to_centimeters(inches_in):
+    print (inches_in, " inches in centimeters is ", inches_in * 2.5400)
 
 
-def convert_kilo_to_pounds():
-    print ""
+def convert_kilo_to_pounds(kilos_in):
+    print (kilos_in, " kilos in pounds is ", kilos_in * 2.2046)
 
 
-def convert_pounds_to_kilos():
-    print ""
+def convert_pounds_to_kilos(pounds_in):
+    print (pounds_in, " pounds in kilos is ", pounds_in * 0.45359)
 
 
 def take_input(conversion):
@@ -37,7 +37,7 @@ speedConversions = ["KphToMph", "MphToKph"]
 lengthConversions = ["CentimetersToInches", "InchesToCentimeters"]
 weightConversions = ["KiloToPounds", "PoundsToKils"]
 while True:
-    print ("Enter The conversion you would like")
+    print ("Enter the number for the conversion you would like")
     printChoicesWithIndexes(conversions)
     choice = input()
     print ("Choice is ", conversions[choice-1])
@@ -46,9 +46,9 @@ while True:
         printChoicesWithIndexes(weightConversions)
 
         if choice == 1:
-            print("1")
+            convert_kilo_to_pounds(take_input("Kilos"))
         elif choice == 2:
-            print("2")
+            convert_pounds_to_kilos((take_input("pounds")))
         else:
             print("Not a valid option")
 
@@ -56,9 +56,9 @@ while True:
         printChoicesWithIndexes(lengthConversions)
 
         if choice == 1:
-            print("1")
+            convert_centimeters_to_inches(take_input("centimeters"))
         elif choice == 2:
-            print("2")
+            convert_inches_to_centimeters(take_input("inches"))
         else:
             print("Not a valid option")
 
@@ -69,7 +69,7 @@ while True:
         if choice == 1:
             convert_kph_to_mph(take_input("Kilometers"))
         elif choice == 2:
-            print("2")
+            convert_mph_to_kph(take_input("Miles"))
         else:
             print("Not a valid option")
 
